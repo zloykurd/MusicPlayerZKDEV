cd4f372c2c86aa50286d1b560e777371b041cdb3
ZLOYKURD\pc <radzhoyan@outlook.com>
2018-04-28 00:37:18 +0600

    Start

---
+++ app/src/main/java/zloykurd/developer/musicplayerzkdev/UI/Activities/StartActivity.java
@@ -0,0 +1,16 @@
+package zloykurd.developer.musicplayerzkdev.UI.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zloykurd.developer.musicplayerzkdev.R;
+
+public class StartActivity extends AppCompatActivity {
+    private static final String TAG = "StartActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
